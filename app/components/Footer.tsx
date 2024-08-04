d3c5d05d3fbfd397c6e9a2d1f02551464ed1bbca
Ethan Sutherland <ESutherland95@gmail.com>
2024-08-04 15:06:05 -0500

    updated footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,10 +3,12 @@ import { CgVercel } from "react-icons/cg";
 
 const Footer = () => {
   return (
-    <footer className="text-center text-zinc-400 text-xs mt-16 mb-4 flex items-center justify-center gap-1">
+    <footer className="text-center text-zinc-400 text-xs hidden sm:flex mt-16 mb-4 items-center justify-center gap-1">
       © 2024 Ethan Sutherland | All Rights Reserved | Made with Next.js,
-      TailwindCSS, and Framer Motion | Powered By Vercel{" "}
-      <CgVercel className="w-5 h-5" />
+      TailwindCSS, and Framer Motion |{" "}
+      <span className=" flex items-center justify-center">
+        Powered By Vercel <CgVercel className="w-5 h-5" />
+      </span>
     </footer>
   );
 };
